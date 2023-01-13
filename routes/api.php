c6cc23a6c8bcc947801020f0fb57fdd699871ffd
Ashish Chawla <ashishchawla@Madmins-MacBook-Pro.local>
2023-01-13 16:18:53 +0530

    Testing Changesg

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::post('/register', 'Auth\ApiAuthController@register');
 Route::post('/login', 'Auth\ApiAuthController@login');
 Route::post('/verifyEmail', 'Auth\ApiAuthController@verifyEmail');
